becc2c014b5eeab11488f12c9ef6fd4183a4bc83
yuanyang <yuanyang@thunder.com.cn>
2020-03-18 17:50:10 +0800

    可扩展的哈希表 更新插入bug

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -8,7 +8,6 @@ class Node(object):
         self.key = key
         self.value = value
         self.hash_key = hash_key
-        self.bin_bash_key = bin(self.hash_key)
 
 
 class Bucket(object):
@@ -52,29 +51,29 @@ class Bucket(object):
     def collision(self, collision_time):
         return collision_time ** 2
 
-    def split_node(self, bucket_index_base, step, old_bucket_index):
-        left = 2 * old_bucket_index
-        right = left + 1
-        left_bucket = Bucket(max_node=self._max_node)
-        right_bucket = Bucket(max_node=self._max_node)
+    def split_node(self, bucket_index_base, step):
+        index = []
+        bucket = []
         for node in self._node_list:
             if not node:
                 continue
             bucket_index = (node.hash_key & bucket_index_base) >> step
             node_index = node.hash_key & self._max_node - 1
-            if bucket_index == left:
-                left_bucket.insert_node(index=node_index, key=node.key, value=node.value, hash_key=node.hash_key)
-            elif bucket_index == right:
-                right_bucket.insert_node(index=node_index, key=node.key, value=node.value, hash_key=node.hash_key)
+            if bucket_index not in index:
+                new_bucket = Bucket(max_node=self._max_node)
+                new_bucket.insert_node(index=node_index, key=node.key, value=node.value, hash_key=node.hash_key)
+                index.append(bucket_index)
+                bucket.append(new_bucket)
             else:
-                assert False, "Bucket wrong"
-        return left, left_bucket, right, right_bucket
+                bucket_num = index.index(bucket_index)
+                bucket[bucket_num].insert_node(index=node_index, key=node.key, value=node.value, hash_key=node.hash_key)
+        return index, bucket
 
 
 class ExtendableHashTable(object):
-    max_bucket = 8
+    max_bucket = 5
 
-    def __init__(self, max_node=17):
+    def __init__(self, max_node=7):
         self.bucket = 1
         self.bucket_num = 1 << 1
         self.bucket_list = [None] * self.bucket_num
@@ -94,7 +93,7 @@ class ExtendableHashTable(object):
     def extend_bucket(self):
         self.bucket += 1
         assert self.bucket <= self.__class__.max_bucket, "超过桶子上限"
-        self.bucket_num = 1 << self.bucket
+        self.bucket_num = self.bucket_num << 1
         new_bucket_list = []
         for bucket in self.bucket_list:
             new_bucket_list += [bucket]
@@ -110,7 +109,7 @@ class ExtendableHashTable(object):
 
     def get_index(self, hash_key):
         bucket_index = (hash_key & self.bucket_index_base) >> self.step
-        node_index = (hash_key & self.max_node) - 1
+        node_index = max((hash_key & self.max_node) - 1, 0)
         return bucket_index, node_index
 
     def insert(self, key, value):
@@ -120,13 +119,26 @@ class ExtendableHashTable(object):
             if not self.bucket_list[bucket_index]:
                 self.bucket_list[bucket_index] = Bucket(max_node=self.max_node)
             if not self.bucket_list[bucket_index].check_status():
+                left_index = bucket_index - 1
+                right_index = bucket_index + 1
                 old_bucket = self.bucket_list[bucket_index]
+                self.bucket_list[bucket_index] = None
+                while left_index >= 0 or right_index < self.bucket_num:
+                    if left_index >= 0 and old_bucket == self.bucket_list[left_index]:
+                        self.bucket_list[left_index] = None
+                        left_index -= 1
+                    else:
+                        left_index = -1
+                    if right_index < self.bucket_num and old_bucket == self.bucket_list[right_index]:
+                        self.bucket_list[right_index] = None
+                        right_index += 1
+                    else:
+                        right_index = self.bucket_num
                 self.extend_bucket()
-                left, left_bucket, right, right_bucket = old_bucket.split_node(bucket_index_base=self.bucket_index_base,
-                                                                               step=self.step,
-                                                                               old_bucket_index=bucket_index)
-                self.bucket_list[left] = left_bucket
-                self.bucket_list[right] = right_bucket
+                bucket_index, bucket = old_bucket.split_node(bucket_index_base=self.bucket_index_base,
+                                                             step=self.step)
+                for i, index in enumerate(bucket_index):
+                    self.bucket_list[index] = bucket[i]
             else:
                 break
         self.bucket_list[bucket_index].insert_node(index=node_index, key=key, value=value, hash_key=hash_key)
@@ -144,13 +156,19 @@ class ExtendableHashTable(object):
         if not self.bucket_list[bucket_index]:
             return None
         node = self.bucket_list[bucket_index].find_node(index=node_index, key=key)
-        return node.value
+        return node
 
 
 if __name__ == "__main__":
     hash_table = ExtendableHashTable()
-    for i in range(65, 96):
+    for i in range(65, 80):
         key = chr(i)
         hash_table.insert(key=key, value=i)
-        assert hash_table.find(key=key) == i, "find 操作不一致"
+    for i in range(65, 80):
+        key = chr(i)
+        node = hash_table.find(key=key)
+        if node is None:
+            print(key)
+        else:
+            assert node.value == i, "find 操作不一致"
     print("end")
